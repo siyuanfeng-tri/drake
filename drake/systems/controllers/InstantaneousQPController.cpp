9576739bbbf15064356e41714e9ac0d9fd4d41c2
Siyuan Feng <siyuan.feng@tri.global>
2016-08-09 15:55:12 -0400

    turn on the min max bounds on Fz

--- drake/systems/controllers/InstantaneousQPController.cpp
+++ drake/systems/controllers/InstantaneousQPController.cpp
@@ -1188,7 +1188,7 @@ int InstantaneousQPController::setupAndSolveQP(
   // limits on some body accelerations
   int n_ineq = 2 * nu + 2 * 6 * desired_body_accelerations.size();
   // add a max and min bound on the normal force per contact
-  // n_ineq += 2 * active_supports.size();
+  n_ineq += 2 * active_supports.size();
   MatrixXd Ain =
       MatrixXd::Zero(n_ineq, nparams);  // note: obvious sparsity here
   VectorXd bin = VectorXd::Zero(n_ineq);
@@ -1232,23 +1232,22 @@ int InstantaneousQPController::setupAndSolveQP(
     constraint_start_index += 6;
   }
 
-  /*
   // contact normal force component stuff
   int beta_start = 0;
   for (size_t c = 0; c < active_supports.size(); c++) {
     const auto& active_support = active_supports[c];
     int active_support_length = nd * active_support.contact_pts.size();
     const auto& B_per_foot = B.middleCols(beta_start, active_support_length);
+    // TODO: this is not rotated. So really just capping Fz in the world frame
     MatrixXd B_rotate = B_per_foot;
 
     double upper = active_support.total_normal_force_upper_bound;
     double lower = active_support.total_normal_force_lower_bound;
 
-    if (upper >= 1.5 * robot->getMass())
-      upper = 1.5 * robot->getMass();
-
-    if (lower <= 0)
+    if (upper >= 1.5 * robot->getMass() * 9.81 || lower < 0 || upper < lower) {
+      upper = 1.5 * robot->getMass() * 9.81;
       lower = 0;
+    }
 
     // max
     Ain.block(constraint_start_index, nq + beta_start, 1, active_support_length) = B_rotate.row(2);
@@ -1261,7 +1260,6 @@ int InstantaneousQPController::setupAndSolveQP(
     std::cout << "foot " << c << " beta start " << beta_start << " active_support_length " << active_support_length << " [u,l] " << active_support.total_normal_force_upper_bound << " " << active_support.total_normal_force_lower_bound << std::endl;
     beta_start += active_support_length;
   }
-  */
 
   for (int i = 0; i < n_ineq; i++) {
     // remove inf constraints---needed by gurobi
