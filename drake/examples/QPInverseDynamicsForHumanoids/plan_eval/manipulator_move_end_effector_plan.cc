1e7668d9f690a90745bcf05faba6c37dc0a503c5
Siyuan Feng <siyuan.feng@tri.global>
2017-03-21 09:07:15 -0400

    WIP

---
+++ drake/examples/QPInverseDynamicsForHumanoids/plan_eval/manipulator_move_end_effector_plan.cc
@@ -0,0 +1,79 @@
+#include "drake/examples/QPInverseDynamicsForHumanoids/plan_eval/manipulator_move_end_effector_plan.h"
+
+#include "drake/lcmt_manipulator_plan_move_end_effector.hpp"
+#include "drake/util/lcmUtil.h"
+
+namespace drake {
+namespace examples {
+namespace qp_inverse_dynamics {
+
+template <typename T>
+void ManipulatorMoveEndEffectorPlan<T>::InitializeGenericPlanDerived(
+    const HumanoidStatus& robot_status, const param_parsers::ParamSet& paramset,
+    const param_parsers::RigidBodyTreeAliasGroups<T>& alias_groups) {
+  std::string ee_name = "end_effector";
+  // Knots are constant, the second time doesn't matter.
+  const std::vector<T> times = {robot_status.time(), robot_status.time() + 1};
+  const RigidBody<T>* ee_body = alias_groups.get_body(ee_name);
+  Isometry3<T> ee_pose = robot_status.robot().CalcBodyPoseInWorldFrame(
+        robot_status.cache(), *ee_body);
+
+  PiecewiseCartesianTrajectory<T> ee_traj =
+      PiecewiseCartesianTrajectory<T>::MakeCubicLinearWithZeroEndVelocity(
+          times, {ee_pose, ee_pose});
+  this->set_body_trajectory(ee_body, ee_traj);
+}
+
+template <typename T>
+void ManipulatorMoveEndEffectorPlan<T>::HandlePlanMessageGenericPlanDerived(
+    const HumanoidStatus& robot_status, const param_parsers::ParamSet& paramset,
+    const param_parsers::RigidBodyTreeAliasGroups<T>& alias_groups,
+    const void* message_bytes, int message_length) {
+  // Tries to decode as a lcmt_manipulator_plan_move_end_effector message.
+  lcmt_manipulator_plan_move_end_effector msg;
+  int consumed = msg.decode(message_bytes, 0, message_length);
+  DRAKE_DEMAND(consumed == message_length);
+
+  DRAKE_DEMAND(static_cast<size_t>(msg.num_steps) == msg.utimes.size() &&
+               static_cast<size_t>(msg.num_steps) == msg.poses.size());
+  DRAKE_DEMAND(msg.num_steps >= 0);
+
+  if (msg.num_steps == 0)
+    return;
+
+  std::vector<T> times;
+  std::vector<Isometry3<T>> poses;
+
+  std::string ee_name = "end_effector";
+  const RigidBody<T>* ee_body = alias_groups.get_body(ee_name);
+
+  // If the first keyframe does not start immediately (its time > 0), we start
+  // from the current desired pose.
+  if (msg.utimes.front() != 0) {
+    times.push_back(robot_status.time());
+    poses.push_back(this->get_body_trajectory(ee_body).get_pose(robot_status.time()));
+  }
+
+  for (int i = 0; i < msg.num_steps; i++) {
+    times.push_back(robot_status.time() + static_cast<T>(msg.utimes[i]) / 1e6);
+    poses.push_back(DecodePose(msg.poses[i]));
+  }
+
+  // TODO(siyuan): use msg.order_of_interpolation.
+  PiecewiseCartesianTrajectory<T> ee_traj =
+      PiecewiseCartesianTrajectory<T>::MakeCubicLinearWithZeroEndVelocity(
+          times, poses);
+
+  this->set_body_trajectory(ee_body, ee_traj);
+}
+
+template <typename T>
+GenericPlan<T>* ManipulatorMoveEndEffectorPlan<T>::CloneGenericPlanDerived() const {
+  return new ManipulatorMoveEndEffectorPlan();
+}
+
+template class ManipulatorMoveEndEffectorPlan<double>;
+
+}  // namespace qp_inverse_dynamics
+}  // namespace examples
+}  // namespace drake
---
+++ drake/examples/QPInverseDynamicsForHumanoids/plan_eval/plan_eval_utils.h
@@ -0,0 +1,203 @@
+#pragma once
+
+#include "drake/common/eigen_types.h"
+#include "drake/common/trajectories/piecewise_polynomial.h"
+#include "drake/common/trajectories/piecewise_quaternion.h"
+#include "drake/multibody/rigid_body_tree.h"
+
+namespace drake {
+namespace examples {
+namespace qp_inverse_dynamics {
+
+/**
+ * ContactState is intended to represent a set of bodies that are in contact.
+ * Detailed information such as contact points, contact wrench are not included
+ * here.
+ */
+typedef std::set<const RigidBody<double>*> ContactState;
+
+template <typename T>
+T average_angles(T a, T b) {
+  T x = fmod(fabs(a - b), 2 * M_PI);
+  if (x >= 0 && x <= M_PI)
+    return fmod((a + b) / 2., 2 * M_PI);
+  else if (x >= M_PI && x < 1.5 * M_PI)
+    return fmod((a + b) / 2., 2 * M_PI) + M_PI;
+  else
+    return fmod((a + b) / 2., 2 * M_PI) - M_PI;
+}
+
+/**
+ * A wrapper class that stores a PiecewisePolynomial and its first and second
+ * derivatives. This class is supposed to represent position, velocity and
+ * acceleration. Thus, when the interpolating time is beyond the time bounds,
+ * the interpolated velocity and acceleration will be set to zero, and the
+ * interpolated position will peg at the terminal values. All dimensions are
+ * assumed to be independent of each other.
+ */
+template <typename T>
+class PiecewiseCubicTrajectory {
+ public:
+  PiecewiseCubicTrajectory() {}
+
+  /**
+   * Constructor.
+   * @param position_traj PiecewisePolynomial that represents the position
+   * trajectory. Its first and second derivatives are computed and stored.
+   */
+  PiecewiseCubicTrajectory(const PiecewisePolynomial<T>& position_traj) {
+    q_ = position_traj;
+    qd_ = q_.derivative();
+    qdd_ = qd_.derivative();
+  }
+
+  /**
+   * Returns the interpolated position at @p time.
+   */
+  VectorX<T> get_position(double time) const {
+    return q_.value(time);
+  }
+
+  /**
+   * Returns the interpolated velocity at @p time or zero if @p time is out of
+   * the time bounds.
+   */
+  VectorX<T> get_velocity(double time) const {
+    if (time <= q_.getStartTime() || time >= q_.getEndTime()) {
+      return VectorX<T>::Zero(q_.rows());
+    }
+    return qd_.value(time);
+  }
+
+  /**
+   * Returns the interpolated acceleration at @p time or zero if @p time is out
+   * of the time bounds.
+   */
+  VectorX<T> get_acceleration(double time) const {
+    if (time <= q_.getStartTime() || time >= q_.getEndTime()) {
+      return VectorX<T>::Zero(q_.rows());
+    }
+    return qdd_.value(time);
+  }
+
+  /**
+   * Returns the start time of this trajectory.
+   */
+  double get_start_time() const { return q_.getStartTime(); }
+
+  /**
+   * Returns the end time of this trajectory.
+   */
+  double get_end_time() const { return q_.getEndTime(); }
+
+ private:
+  PiecewisePolynomial<T> q_;
+  PiecewisePolynomial<T> qd_;
+  PiecewisePolynomial<T> qdd_;
+};
+
+/**
+ * A wrapper class that represents a Cartesian trajectory, whose position part
+ * is a PiecewisePolynomial, and the rotation part is a
+ * PiecewiseQuaternionSlerp.
+ */
+template <typename T>
+class PiecewiseCartesianTrajectory {
+ public:
+  /**
+   * Constructs a PiecewiseCartesianTrajectory from given @p time and @p poses.
+   * A cubic polynomial is used to construct the position part. There must be
+   * at least two elements in @p times and @p poses.
+   * If @p zero_end_point_linear_velocities is true, zero velocities are set
+   * for the linear velocity. When @p zero_end_point_linear_velocities is
+   * false and there are only two elements in @p time, a linear spline is used
+   * to represent the position part. PiecewiseQuaternionSlerp is used for the
+   * orientation part.
+   * @param times Breaks used to build the splines.
+   * @param poses Knots used to build the splines.
+   * @param zero_end_point_linear_velocities If true, the start and end
+   * velocity for the position spline are set to zero.
+   */
+  static PiecewiseCartesianTrajectory<T> MakeCubicLinearWithZeroEndVelocity(
+      const std::vector<T>& times,
+      const std::vector<Isometry3<T>>& poses) {
+    std::vector<MatrixX<T>> pos_knots(poses.size());
+    eigen_aligned_std_vector<Matrix3<T>> rot_knots(poses.size());
+    for (size_t i = 0; i < poses.size(); ++i) {
+      pos_knots[i] = poses[i].translation();
+      rot_knots[i] = poses[i].linear();
+    }
+
+    return PiecewiseCartesianTrajectory(
+        PiecewisePolynomial<T>::Cubic(
+            times, pos_knots, MatrixX<T>::Zero(3, 1), MatrixX<T>::Zero(3, 1)),
+        PiecewiseQuaternionSlerp<T>(times, rot_knots));
+  }
+
+  PiecewiseCartesianTrajectory(const PiecewisePolynomial<T>& pos_traj,
+                               const PiecewiseQuaternionSlerp<T>& rot_traj) {
+    position_ = pos_traj;
+    positiond_ = position_.derivative();
+    positiondd_ = positiond_.derivative();
+    orientation_ = rot_traj;
+  }
+
+  /**
+   * Returns the interpolated pose at @p time.
+   */
+  Isometry3<T> get_pose(double time) const {
+    Isometry3<T> pose;
+    pose.translation() = position_.value(time);
+    pose.linear() = orientation_.orientation(time).toRotationMatrix();
+    return pose;
+  }
+
+  /**
+   * Returns the interpolated velocity at @p time or zero if @p time is before
+   * this trajectory's start time or after its end time.
+   */
+  Vector6<T> get_velocity(double time) const {
+    if (time <= position_.getStartTime() || time >= position_.getEndTime()) {
+      return Vector6<T>::Zero();
+    }
+    Vector6<T> velocity;
+    velocity.template head<3>() = orientation_.angular_velocity(time);
+    velocity.template tail<3>() = positiond_.value(time);
+    return velocity;
+  }
+
+  /**
+   * Returns the interpolated acceleration at @p time or zero if @p time is
+   * before this trajectory's start time or after its end time.
+   */
+  Vector6<T> get_acceleration(double time) const {
+    if (time <= position_.getStartTime() || time >= position_.getEndTime()) {
+      return Vector6<T>::Zero();
+    }
+    Vector6<T> acceleration;
+    acceleration.template head<3>() = orientation_.angular_acceleration(time);
+    acceleration.template tail<3>() = positiondd_.value(time);
+    return acceleration;
+  }
+
+  /**
+   * Returns the start time of this trajectory.
+   */
+  double get_start_time() const { return position_.getStartTime(); }
+
+  /**
+   * Returns the end time of this trajectory.
+   */
+  double get_end_time() const { return position_.getEndTime(); }
+
+ private:
+  PiecewisePolynomial<T> position_;
+  PiecewisePolynomial<T> positiond_;
+  PiecewisePolynomial<T> positiondd_;
+
+  PiecewiseQuaternionSlerp<T> orientation_;
+};
+
+}  // namespace qp_inverse_dynamics
+}  // namespace examples
+}  // namespace drake
