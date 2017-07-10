dfbfd7dba066c6e74825d3544ab2ada19565996b
Siyuan Feng <siyuan.feng@tri.global>
2017-07-10 17:50:57 -0400

    threaded it

--- drake/examples/kuka_iiwa_arm/jjz_controller.cc
+++ drake/examples/kuka_iiwa_arm/jjz_controller.cc
@@ -24,7 +24,6 @@ namespace {
 
 const char* const kLcmStatusChannel = "IIWA_STATUS";
 const char* const kLcmCommandChannel = "IIWA_COMMAND";
-// const char* const kLcmPlanChannel = "COMMITTED_ROBOT_PLAN";
 
 constexpr double kUninitTime = -1.0;
 const std::string kPath =
@@ -101,22 +100,24 @@ class RobotPlanRunner {
                   const Isometry3<double>& X_WB)
       : jaco_planner_(model_path, end_effector_name, X_WB),
         robot_(jaco_planner_.get_robot()),
-        state_(robot_) {
+        q_d_(VectorX<double>::Zero(robot_.get_num_positions())) {
     VerifyIiwaTree(robot_);
     lcm_.subscribe(kLcmStatusChannel, &RobotPlanRunner::HandleStatus, this);
-    // lcm_.subscribe(kLcmPlanChannel,
-    //                &RobotPlanRunner::HandlePlan, this);
   }
 
   PiecewisePolynomial<double> SplineToDesiredConfiguration(
-      const VectorX<double>& q_d, double duration) const {
-    std::vector<double> times = {state_.get_time(), state_.get_time() + duration};
-    std::vector<MatrixX<double>> knots = {state_.get_q(), q_d};
-    MatrixX<double> zero = MatrixX<double>::Zero(robot_.get_num_velocities(), 1);
+      const VectorX<double>& q0, const VectorX<double>& q_d,
+      double t0, double duration) const {
+    std::vector<double> times = {t0, t0 + duration};
+    std::vector<MatrixX<double>> knots = {q0, q_d};
+    MatrixX<double> zero =
+        MatrixX<double>::Zero(robot_.get_num_velocities(), 1);
     return PiecewisePolynomial<double>::Cubic(times, knots, zero, zero);
   }
 
-  void GenerateEETraj(const VectorX<double>& q, double r, double period, std::vector<double>* times, std::vector<Isometry3<double>>* poses) const {
+  void GenerateEETraj(const VectorX<double>& q, double r, double period,
+                      std::vector<double>* times,
+                      std::vector<Isometry3<double>>* poses) const {
     double dt = 1e-3;
     const int N = std::ceil(period / dt);
     times->resize(N);
@@ -125,31 +126,28 @@ class RobotPlanRunner {
     KinematicsCache<double> cache = robot_.CreateKinematicsCache();
     cache.initialize(q);
     robot_.doKinematics(cache);
-    Isometry3<double> X_WE0 = robot_.CalcBodyPoseInWorldFrame(cache,
-        jaco_planner_.get_end_effector());
+    Isometry3<double> X_WE0 = robot_.CalcBodyPoseInWorldFrame(
+        cache, jaco_planner_.get_end_effector());
 
     for (int i = 0; i < N; ++i) {
       (*times)[i] = (i + 1) * dt;
       (*poses)[i] = X_WE0;
-      (*poses)[i].translation()[0] -= r * (std::cos(2 * M_PI * times->at(i) / period) - 1);
-      (*poses)[i].translation()[1] += r * std::sin(2 * M_PI * times->at(i) / period);
+      (*poses)[i].translation()[0] -=
+          r * (std::cos(2 * M_PI * times->at(i) / period) - 1);
+      (*poses)[i].translation()[1] +=
+          r * std::sin(2 * M_PI * times->at(i) / period);
     }
   }
 
-  void Run() {
-    lcmt_iiwa_command iiwa_command;
-    iiwa_command.num_joints = robot_.get_num_positions();
-    iiwa_command.joint_position.resize(robot_.get_num_positions(), 0.);
-    iiwa_command.num_torques = 0;
-    iiwa_command.joint_torque.resize(robot_.get_num_positions(), 0.);
-
-    double cmd_time = kUninitTime;
+  void ControlThread() {
+    IiwaState state(robot_);
+    lcmt_iiwa_status msg;
 
     constexpr int GOTO = 0;
     constexpr int CIRCLE = 1;
     constexpr int NOOP = 2;
     constexpr int FB = 3;
-//    constexpr int WAIT = 3;
+    //    constexpr int WAIT = 3;
 
     int STATE = GOTO;
     bool state_init = true;
@@ -166,14 +164,21 @@ class RobotPlanRunner {
     size_t q_ctr = 0;
 
     VectorX<double> q_nominal = robot_.getZeroConfiguration();
-
     KinematicsCache<double> cc = robot_.CreateKinematicsCache();
 
+    VectorX<double> q_cmd;
+
     while (true) {
-      // Call lcm handle until at least one status message is
-      // processed.
-      while (0 == lcm_.handleTimeout(10) || state_.get_time() == cmd_time) {
+      // TODO lock this
+      {
+        std::lock_guard<std::mutex> guard(state_lock_);
+        msg = iiwa_status_;
       }
+
+      if (msg.utime == -1)
+        continue;
+
+      state.UpdateState(msg);
 
       switch (STATE) {
         // go home.
@@ -184,14 +189,14 @@ class RobotPlanRunner {
             q1[1] += 45. * M_PI / 180.;
             q1[3] -= M_PI / 2.;
             q1[5] += 45. * M_PI / 180.;
-            traj = SplineToDesiredConfiguration(q1, 2);
+            traj = SplineToDesiredConfiguration(state.get_q(), q1, state.get_time(), 2);
             state_init = false;
-            state_t0 = state_.get_time();
+            state_t0 = state.get_time();
           }
 
-          q_d_ = traj.value(state_.get_time());
+          q_cmd = traj.value(state.get_time());
 
-          if (state_.get_time() - state_t0 > 2.1) {
+          if (state.get_time() - state_t0 > 2.1) {
             STATE = FB;
             state_init = true;
           }
@@ -211,10 +216,10 @@ class RobotPlanRunner {
             q_ctr = 0;
 
             state_init = false;
-            state_t0 = state_.get_time();
+            state_t0 = state.get_time();
           }
 
-          q_d_ = q_sol[q_ctr++];
+          q_cmd = q_sol[q_ctr++];
 
           if (q_ctr >= q_sol.size()) {
             STATE = NOOP;
@@ -233,32 +238,32 @@ class RobotPlanRunner {
 
             GenerateEETraj(q1, 0.1, 1.0, &times, &poses);
             q_ctr = 0;
-            cc.initialize(state_.get_q());
+            cc.initialize(state.get_q());
             robot_.doKinematics(cc);
 
             state_init = false;
-            state_t0 = state_.get_time();
+            state_t0 = state.get_time();
           }
 
           const auto& pose_d = poses[q_ctr++];
-          double dt = 1e-3;
+          double dt = 3e-3;
 
-          Isometry3<double> X_WE =
-              robot_.CalcBodyPoseInWorldFrame(cc,
-                  jaco_planner_.get_end_effector());
+          Isometry3<double> X_WE = robot_.CalcBodyPoseInWorldFrame(
+              cc, jaco_planner_.get_end_effector());
 
           Vector6<double> V_WE_d =
               jaco_planner_.ComputePoseDiffInWorldFrame(X_WE, pose_d) / dt;
-          VectorX<double> v = jaco_planner_.ComputeDofVelocity(cc, V_WE_d, q_nominal, dt);
-          q_d_ = cc.getQ() + v * dt;
-          cc.initialize(q_d_);
+          VectorX<double> v =
+              jaco_planner_.ComputeDofVelocity(cc, V_WE_d, q_nominal, dt);
+          cc.initialize(cc.getQ() + v * dt);
           robot_.doKinematics(cc);
 
-          if (q_ctr >= poses.size()) {
+          q_cmd = cc.getQ();
+
+          if (q_ctr >= poses.size())
             q_ctr = 0;
-            //STATE = NOOP;
-            //state_init = true;
-          }
+
+          break;
         }
 
         case NOOP: {
@@ -266,32 +271,73 @@ class RobotPlanRunner {
         }
       }
 
+      // TODO lock this:
+      {
+        std::lock_guard<std::mutex> guard(cmd_lock_);
+        q_d_ = q_cmd;
+      }
+    }
+  }
+
+  void Run() {
+    lcmt_iiwa_command iiwa_command;
+    iiwa_command.num_joints = robot_.get_num_positions();
+    iiwa_command.joint_position.resize(robot_.get_num_positions(), 0.);
+    iiwa_command.num_torques = 0;
+    iiwa_command.joint_torque.resize(robot_.get_num_positions(), 0.);
+
+    iiwa_status_.utime = -1;
+    int64_t cmd_time = -1;
+
+    std::thread control_thread =
+        std::thread(&RobotPlanRunner::ControlThread, this);
+
+    while (true) {
+      // Call lcm handle until at least one status message is
+      // processed.
+      while (0 == lcm_.handleTimeout(10) || iiwa_status_.utime == cmd_time) {
+      }
+
+      // Initialize q_d_ to measured q.
+      if (cmd_time == -1) {
+        for (int i = 0; i < robot_.get_num_positions(); i++) {
+          q_d_[i] = iiwa_status_.joint_position_measured[i];
+        }
+      }
+
       // Make msg.
-      iiwa_command.utime = static_cast<int64_t>(state_.get_time() * 1e6);
-      for (int i = 0; i < robot_.get_num_positions(); i++) {
-        iiwa_command.joint_position[i] = q_d_[i];
+      iiwa_command.utime = iiwa_status_.utime;
+      {
+        std::lock_guard<std::mutex> guard(cmd_lock_);
+        for (int i = 0; i < robot_.get_num_positions(); i++) {
+          // TODO lock this.
+          iiwa_command.joint_position[i] = q_d_[i];
+        }
       }
 
       // Send command
       lcm_.publish(kLcmCommandChannel, &iiwa_command);
       // Log time of command.
-      cmd_time = state_.get_time();
+      cmd_time = iiwa_status_.utime;
     }
   }
 
  private:
   void HandleStatus(const lcm::ReceiveBuffer*, const std::string&,
                     const lcmt_iiwa_status* status) {
-    // iiwa_status_ = *status;
-    state_.UpdateState(*status);
+    std::lock_guard<std::mutex> guard(state_lock_);
+    iiwa_status_ = *status;
   }
 
   lcm::LCM lcm_;
   manipulation::planner::JacobianIk jaco_planner_;
   const RigidBodyTree<double>& robot_;
-  // lcmt_iiwa_status iiwa_status_;
-  IiwaState state_;
+  lcmt_iiwa_status iiwa_status_{};
+  // IiwaState state_;
   VectorX<double> q_d_;
+
+  std::mutex state_lock_;
+  std::mutex cmd_lock_;
 };
 
 int do_main() {
